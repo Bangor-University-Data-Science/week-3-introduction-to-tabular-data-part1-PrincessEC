8a40eb6a19fa4be82a48239c8244c80b5437525d
Princess Chibuzor Enyagam <prn24snl@bangor.ac.uk>
2024-10-25 15:51:47 +0100

    Assignment3.91

--- titanic_analysis/summary_table.py
+++ titanic_analysis/summary_table.py
@@ -14,10 +14,10 @@ def create_summary_table(df):
      # Implement the logic here
     
     summary = {
-        "Feature Name": df.columns,
-        "Data Type":[df[col].dtype for col in df.columns],
-        "Unique Values": [int(df[col].nunique()) for col in df.columns],
-        "Missing Values": [bool(df[col].isnull().any()) for col in df.columns]
+        "Feature Name": df.columns.tolist(),
+        "Data Type":[df[col].dtype for col in df.columns.tolist()],
+        "Unique Values": [df[col].nunique() for col in df.columns.tolist()],
+        "Missing Values": [df[col].isnull().any() for col in df.columns.tolist()]
     }
 
     return pd.DataFrame(summary, columns=["Feature Name", "Data Type", "unique Values", "Missing Values"])
